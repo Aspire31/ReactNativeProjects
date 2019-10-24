0e89c95ee641b82ec6a6c8e20db423e63f6a9964
Alisha Nagpal <alisha.nagpal31@yahoo.in>
2019-10-24 13:04:55 +0530

    add

--- UIScreen24Oct/PrizeView.js
+++ UIScreen24Oct/PrizeView.js
@@ -7,6 +7,10 @@ export const PrizeView = React.memo(function pureFunction() {
       <View style={styles.mainView} >
           <View style={styles.backgroundView} >
 
+              <FlatList 
+              
+              />
+
           </View>
       </View>
     );
@@ -29,18 +33,22 @@ const styles = StyleSheet.create({
 
 const DATA = [
     {
-        imageURL = 'https://ichef.bbci.co.uk/news/660/cpsprodpb/F76F/production/_101934336_hi001487062.jpg',
-        text = '1st Prize',
-        badge = require('./Images/first.png')
+        imageURL: require('./Images/hippo.jpg'),
+        text: '1st Prize',
+        badge: require('./Images/first.png')
     },
     {
-        imageURL = 'https://www.asiaone.com/sites/default/files/original_images/Oct2019/20191003_foxes_pixabay.jpg',
-        text = '1st Prize',
-        badge = require('./Images/second.png')
+        imageURL: require('./Images/fox.jpg'),
+        text: '1st Prize',
+        badge: require('./Images/second.png')
     },
     {
-        imageURL = 'https://media.wired.com/photos/5d3646cc65c5e400082d459a/master/w_2560%2Cc_limit/Sceince_Adaptation-130859883.jpg',
-        text = '1st Prize',
-        badge = require('./Images/third.png')
+        imageURL: require('./Images/bird.jpg'),
+        text: '1st Prize',
+        badge: require('./Images/third.png')
     }
 ]
+
+
+
+
